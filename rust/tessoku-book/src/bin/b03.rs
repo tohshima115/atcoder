2ad10b2706beff610be8abcc4aa01f71c13354dd
tohshima115 <tohshima115@gmail.com>
2026-05-31 13:29:47 +0900

    checkpoint: 20260531-132947

--- rust/tessoku-book/src/bin/b03.rs
+++ rust/tessoku-book/src/bin/b03.rs
@@ -1,8 +1,20 @@
 use proconio::input;
+use std::collections::HashSet;
 
 fn main() {
     input! {
         n: usize,
+        a: [i64; n],
     }
-    println!("{}", n);
+    let mut ans = false;
+    let a_set: HashSet<i64> = a.iter().cloned().collect();
+    'outer: for &v1 in &a{
+        for &v2 in &a{
+            if a_set.contains(&(& 1000 - (v1 + v2))){
+                ans = true;
+                break `outer;
+            }
+        } 
+    }
+    println!("{}", if ans {"Yes"} else {"No"});
 }
