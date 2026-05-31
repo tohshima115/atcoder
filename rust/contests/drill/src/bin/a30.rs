b4ed6ecf537e96da43d7c04dced52353acb67032
tohshima115 <tohshima115@gmail.com>
2026-06-01 03:19:30 +0900

    checkpoint: 20260601-031930

--- rust/contests/drill/src/bin/a30.rs
+++ rust/contests/drill/src/bin/a30.rs
@@ -6,11 +6,12 @@ fn main() {
         k: i64,
         a: [i64; n],
     }
-    let mut count:i64 = 0;
-    for &v in &a{
-        if v >= k {
-            count += 1;
-        };
-    };
+    // let mut count:i64 = 0;
+    // for &v in &a{
+    //     if v >= k {
+    //         count += 1;
+    //     };
+    // };
+    let count: usize = a.iter().filter(|&&x| x >= k).count();
     println!("{}", count);
 }
