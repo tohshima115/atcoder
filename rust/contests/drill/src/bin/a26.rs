35a271f914ed325cead09564495e09cae10ec8d4
tohshima115 <tohshima115@gmail.com>
2026-06-01 02:52:18 +0900

    checkpoint: 20260601-025218

--- rust/contests/drill/src/bin/a26.rs
+++ rust/contests/drill/src/bin/a26.rs
@@ -5,7 +5,7 @@ fn main() {
         n: usize,
         a:[i64; n],
     }
-    let sum = a.iter().sum::<i64>();
+    let sum: i64 = a.iter().sum::<i64>();
     let ave: f64 = sum as f64 / n as f64;
     println!("{:.2}", ave)
 }
--- rust/contests/drill/src/bin/a27.rs
+++ rust/contests/drill/src/bin/a27.rs
@@ -2,5 +2,9 @@ use proconio::input;
 
 fn main() {
     input! {
+        b: i64,
+        h: i64,
     }
+    let size: f64 = b as f64 * h as f64 / 2.0;
+    println!("{:.1}", size);
 }
