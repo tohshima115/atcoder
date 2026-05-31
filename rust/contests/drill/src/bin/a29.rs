fc84fecf6d214b5d082277ed6b601d90b903d5b5
tohshima115 <tohshima115@gmail.com>
2026-06-01 03:07:34 +0900

    checkpoint: 20260601-030734

--- rust/contests/drill/src/bin/a29.rs
+++ rust/contests/drill/src/bin/a29.rs
@@ -2,5 +2,11 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        k: usize,
+        mut a: [i64; n],
     }
+    a.sort();
+    let ans: i64 = a[(a.len() - k)..].iter().sum::<i64>();
+    println!("{}", ans);
 }
