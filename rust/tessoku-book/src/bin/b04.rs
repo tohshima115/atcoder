673746d121ede5052e3387b801787629b06da8e8
tohshima115 <tohshima115@gmail.com>
2026-05-31 14:16:53 +0900

    checkpoint: 20260531-141653

--- rust/tessoku-book/src/bin/b04.rs
+++ rust/tessoku-book/src/bin/b04.rs
@@ -2,7 +2,12 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
+        n: i64,
     }
-    println!("{}", n);
+    let bit[i64] = &n.iter.
+    let mut ans = i64::new;
+    for i in 0..bit.length{
+        ans =+ bit[i];
+    }
+    println!("{}", ans);
 }
