1dd777eec65dd19eb596e2cf0ca41727f4345eab
tohshima115 <tohshima115@gmail.com>
2026-06-01 00:07:56 +0900

    checkpoint: 20260601-000756

--- rust/contests/drill/src/bin/a20.rs
+++ rust/contests/drill/src/bin/a20.rs
@@ -3,18 +3,14 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        a: [i64, n]
+        a: [i64; n]
     }
-    let ans = if n == 1 {
-        0
-    }else{
-        let mut max:i64 = 0;
-        for i in 0..(a.len()-1){
-            if (a[i] - a[i+1]) * (a[i] - a[i+1]) > max{
-                max = (a[i] - a[i+1]) * (a[i] - a[i+1])
-            };
-        };
-        sqer(max)
+    let mut max:i64 = 0;
+    for i in 0..(a.len()-1){
+        let diff = (a[i] - a[i+1]).abs();
+        if diff > max {
+            max = diff
+        }
     };
-    println!("{}", ans);
+    println!("{}", max);
 }
