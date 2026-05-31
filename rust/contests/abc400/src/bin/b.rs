aa72ff5f8fbcd1b0c073942ebfbfb130f1842826
tohshima115 <tohshima115@gmail.com>
2026-06-01 02:02:17 +0900

    checkpoint: 20260601-020216

--- rust/contests/abc400/src/bin/b.rs
+++ rust/contests/abc400/src/bin/b.rs
@@ -6,13 +6,13 @@ fn main() {
         m: i64,
     }
     let mut ans:i64 = 0;
-    let mut isInf: bool = false;
     for i in 0..=m{
         ans += n.pow(i as u32);
         if ans > 10_i64.pow(9 as u32){
-            isInf = true;
+            println!("inf");
             break;
+        }else if i == m{
+            print!("{}", ans);
         };
     };
-    println!("{}", if isInf {"inf"} else {ans});
 }
---
+++ target/debug/abc400-b.d
@@ -0,0 +1,1 @@
+/home/tohshima/project/atcoder/target/debug/abc400-b: /home/tohshima/project/atcoder/rust/contests/abc400/src/bin/b.rs
