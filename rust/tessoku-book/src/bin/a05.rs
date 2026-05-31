055c203e89502fc494568e698f9e14cb51aecb1e
tohshima115 <tohshima115@gmail.com>
2026-05-31 14:52:49 +0900

    checkpoint: 20260531-145249

--- rust/tessoku-book/src/bin/a05.rs
+++ rust/tessoku-book/src/bin/a05.rs
@@ -2,7 +2,18 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
+        n: i64,
+        k: i64,
     }
-    println!("{}", n);
+    let mut ans: i64 = 0;
+    for red in 0..n{
+        if k - red >= 2 {
+            if k - red <= n {
+                ans += k - red -1;
+            }elif k - red >= 2 * n{
+                ans += 2 * n - (k - red) + 1
+            }
+        }
+    }
+    println!("{}", ans);
 }
