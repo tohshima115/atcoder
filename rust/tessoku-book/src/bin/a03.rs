e4661d0858f31b0bc2e88a35388b7cf7c8b3c3dd
tohshima115 <tohshima115@gmail.com>
2026-05-31 10:06:48 +0900

    checkpoint: 20260531-100648

--- rust/tessoku-book/src/bin/a03.rs
+++ rust/tessoku-book/src/bin/a03.rs
@@ -3,6 +3,18 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
+        k: i64,
+        p: [i64; n],
+        q: [i64; n],
     }
-    println!("{}", n);
+    let mut ans = false;
+    for &vp in &p{
+        for &vq in &q{
+            if vp + vq == k {
+                ans = true;
+                break;
+            };
+        };
+    };
+    println!("{}", if ans == true {"Yes"} else {"No"});
 }
