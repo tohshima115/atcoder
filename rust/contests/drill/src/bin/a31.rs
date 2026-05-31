904b85aa037f0d36dd87fa336ba78e60356e829f
tohshima115 <tohshima115@gmail.com>
2026-06-01 03:22:20 +0900

    checkpoint: 20260601-032220

--- rust/contests/drill/src/bin/a31.rs
+++ rust/contests/drill/src/bin/a31.rs
@@ -2,5 +2,9 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        a: [i64; n],
     }
+    let count = a.iter().filter(|&&x| x % 2 == 0).count();
+    println!("{}", count);
 }
