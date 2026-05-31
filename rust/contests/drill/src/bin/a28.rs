e8974c0aa99476d6d827bf7a455541c590abc011
tohshima115 <tohshima115@gmail.com>
2026-06-01 02:58:12 +0900

    checkpoint: 20260601-025812

--- rust/contests/drill/src/bin/a28.rs
+++ rust/contests/drill/src/bin/a28.rs
@@ -3,12 +3,13 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        a: [i64; n],
+        mut a: [i64; n],
     }
+    a.sort();
     let mid: f64 = if n % 2 == 0 {
-        (a[n / 2] as f64 + a[n / 2 + 1] as f64) / 2.0;
+        (a[n / 2 - 1] as f64 + a[n / 2] as f64) / 2.0
     }else{
-        a[(n+1)/2] as f64;
+        a[(n-1)/2] as f64
     };
     println!("{}", mid);
 }
