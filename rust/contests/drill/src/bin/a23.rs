484e3587f734a67d3125f836aa6adddaa2e5cbdf
tohshima115 <tohshima115@gmail.com>
2026-06-01 01:14:05 +0900

    checkpoint: 20260601-011404

--- rust/contests/drill/src/bin/a23.rs
+++ rust/contests/drill/src/bin/a23.rs
@@ -2,5 +2,14 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: i64,
     }
+    let mut ans: i64 = 0;
+    let mut m: i64 = 0;
+    for i in (0..18).rev(){
+        let a = (n - m) / 10_i64.pow(i as u32);
+        ans += a;
+        m += a * 10_i64.pow(i as u32);
+    };
+    println!("{}", ans);
 }
