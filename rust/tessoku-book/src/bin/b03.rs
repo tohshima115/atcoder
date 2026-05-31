2f33feab17ec25d5d11f6f8beb49965da1ac89d4
tohshima115 <tohshima115@gmail.com>
2026-05-31 13:43:10 +0900

    checkpoint: 20260531-134310

--- rust/tessoku-book/src/bin/b03.rs
+++ rust/tessoku-book/src/bin/b03.rs
@@ -6,13 +6,13 @@ fn main() {
         a: [i64; n],
     }
     let mut ans = false;
-    for &v1 in &a{
+    'outer: for &v1 in &a{
         for &v2 in &a{
             if v1 > v2 {
                 for &v3 in &a{
                     if v2 > v3 && v1 + v2 + v3 == 1000{
                         ans = true;
-                        break;
+                        break 'outer;
                     }
                 }
             }
