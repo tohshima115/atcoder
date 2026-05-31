fb863967d456c7bffd3addd70151aef319b43971
tohshima115 <tohshima115@gmail.com>
2026-06-01 03:30:05 +0900

    checkpoint: 20260601-033005

--- rust/contests/drill/src/bin/a32.rs
+++ rust/contests/drill/src/bin/a32.rs
@@ -2,5 +2,9 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: i64,
     }
+    let vec: Vec<i64> = (1..=n).collect();
+    let count = vec.iter().filter(|&&x| x % 3 != 0 && x % 5 != 0).count();
+    println!("{}", count);
 }
