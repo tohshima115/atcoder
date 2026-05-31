69f95b4af05e611d5faa8f069d61900ecccb4d3a
tohshima115 <tohshima115@gmail.com>
2026-06-01 01:32:43 +0900

    checkpoint: 20260601-013243

--- rust/contests/drill/src/bin/a24.rs
+++ rust/contests/drill/src/bin/a24.rs
@@ -2,5 +2,8 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: i64,
     }
+    let ans = n.to_string().chars().map(|c| c.to_digit(10).unwrap() as i64).max();
+    println!("{}", ans);
 }
