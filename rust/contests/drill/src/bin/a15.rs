408127cbb3394ee0fb7e7ec90c2e36c0df2a856d
tohshima115 <tohshima115@gmail.com>
2026-05-31 22:36:51 +0900

    checkpoint: 20260531-223651

--- rust/contests/drill/src/bin/a15.rs
+++ rust/contests/drill/src/bin/a15.rs
@@ -2,5 +2,17 @@ use proconio::input;
 
 fn main() {
     input! {
+        m: usize,
     }
+    let mut season = String::new();
+    if 3 <= m && m <= 5 {
+        season = "spring".to_string();
+    }else if 6 <= m && m <= 8 {
+        season = "summer".to_string();
+    }else if 9 <= m && m <= 11 {
+        season = "autumn".to_string();
+    }else{
+        season = "winter".to_string();
+    }
+    println!("{}", season);
 }
