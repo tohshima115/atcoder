4909d5060d9e5f997c286ddeda5b4f242bdf2437
tohshima115 <tohshima115@gmail.com>
2026-06-01 03:14:54 +0900

    checkpoint: 20260601-031454

--- rust/contests/drill/src/bin/a30.rs
+++ rust/contests/drill/src/bin/a30.rs
@@ -2,5 +2,15 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        k: i64,
+        a: [i64; n],
     }
+    let mut count:i64 = 0;
+    for &v in &a{
+        if v >= k {
+            count += 1;
+        };
+    };
+    println!("{}", count);
 }
