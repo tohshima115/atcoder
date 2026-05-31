2378db68a7d63780bd4d6db39fcb1c4c2d5b0a2b
tohshima115 <tohshima115@gmail.com>
2026-06-01 00:01:26 +0900

    checkpoint: 20260601-000126

--- rust/contests/drill/src/bin/a19.rs
+++ rust/contests/drill/src/bin/a19.rs
@@ -2,5 +2,15 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        a:[i64; n],
     }
+    let ans = if a[0] >= a[n-1] {
+        a[0] - a[n-1]
+    }else{
+        a[n-1] - a[0]
+    };
+    println!("{}", ans);
 }
+
+// これでいいの…？
--- rust/contests/drill/src/bin/a20.rs
+++ rust/contests/drill/src/bin/a20.rs
@@ -2,5 +2,19 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        a: [i64, n]
     }
+    let ans = if n == 1 {
+        0
+    }else{
+        let mut max:i64 = 0;
+        for i in 0..(a.len()-1){
+            if (a[i] - a[i+1]) * (a[i] - a[i+1]) > max{
+                max = (a[i] - a[i+1]) * (a[i] - a[i+1])
+            };
+        };
+        sqer(max)
+    };
+    println!("{}", ans);
 }
