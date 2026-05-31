bb5a7ebf46b047b364d52e0ecf18101d1f35aad3
tohshima115 <tohshima115@gmail.com>
2026-05-31 16:05:32 +0900

    checkpoint: 20260531-160532

--- rust/abc351/src/bin/a.rs
+++ rust/abc351/src/bin/a.rs
@@ -6,9 +6,9 @@ fn main() {
         h: [i64; n]
     }
     let mut ans: i64 = -1;
-    for i in 2..=n {
-        if h[1] > h[i]{
-            ans = i::i64;
+    for i in 1..=n {
+        if h[0] < h[i]{
+            ans = i as i64 + 1;
             break;
         }
     }
