059aa00dd4a95fcf4a00bfef34abd03a704cbb95
tohshima115 <tohshima115@gmail.com>
2026-06-01 00:53:08 +0900

    checkpoint: 20260601-005308

--- rust/contests/drill/src/bin/a22.rs
+++ rust/contests/drill/src/bin/a22.rs
@@ -2,5 +2,14 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: i64,
     }
+    let mut ans: i64 = 0;
+    for i in (0..18).revers(){
+        if [n / 10.dat(i)] >= 1{
+            ans = i;
+            break;
+        }
+    };
+    println("{}", ans);
 }
