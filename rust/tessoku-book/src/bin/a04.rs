aa883644ad36c8a74f247836f723b2f0a5912b6b
tohshima115 <tohshima115@gmail.com>
2026-05-31 14:07:05 +0900

    checkpoint: 20260531-140705

--- rust/tessoku-book/src/bin/a04.rs
+++ rust/tessoku-book/src/bin/a04.rs
@@ -4,10 +4,10 @@ fn main() {
     input! {
         n: i64,
     }
-    let mut ans:string = "";
-    for i in 0..10.rev({
+    let mut ans: String = String::new();
+    for i in 0..10.rev(){
         let bit = (n / (1 << i)) % 2;
-        ans.push_str(bit);
+        ans.push_str(&bit.to_string());
     }
-    println!("{}", n);
+    println!("{}", ans);
 }
