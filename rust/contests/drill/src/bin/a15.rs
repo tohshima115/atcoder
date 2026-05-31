dc2988a34510b4a15fe6eeb3421a8f544683310f
tohshima115 <tohshima115@gmail.com>
2026-05-31 22:41:52 +0900

    checkpoint: 20260531-224152

--- rust/contests/drill/src/bin/a15.rs
+++ rust/contests/drill/src/bin/a15.rs
@@ -4,15 +4,11 @@ fn main() {
     input! {
         m: usize,
     }
-    let mut season = String::new();
-    if 3 <= m && m <= 5 {
-        season = "spring".to_string();
-    }else if 6 <= m && m <= 8 {
-        season = "summer".to_string();
-    }else if 9 <= m && m <= 11 {
-        season = "autumn".to_string();
-    }else{
-        season = "winter".to_string();
-    }
+    let season = match m {
+        3..=5 => "spring",
+        6..=8 => "summer",
+        9..=11 => "autumn",
+        _=> "winter",
+    };
     println!("{}", season);
 }
