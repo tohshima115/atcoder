6557831abde3816475713eda62076acf5f7e059d
tohshima115 <tohshima115@gmail.com>
2026-06-01 01:20:05 +0900

    checkpoint: 20260601-012005

--- rust/contests/drill/src/bin/a23.rs
+++ rust/contests/drill/src/bin/a23.rs
@@ -4,12 +4,13 @@ fn main() {
     input! {
         n: i64,
     }
-    let mut ans: i64 = 0;
-    let mut m: i64 = 0;
-    for i in (0..18).rev(){
-        let a = (n - m) / 10_i64.pow(i as u32);
-        ans += a;
-        m += a * 10_i64.pow(i as u32);
-    };
+    // let mut ans: i64 = 0;
+    // let mut m: i64 = 0;
+    // for i in (0..18).rev(){
+    //     let a = (n - m) / 10_i64.pow(i as u32);
+    //     ans += a;
+    //     m += a * 10_i64.pow(i as u32);
+    // };
+    let ans = n.to_string().chars().map(|c| c.to_digit(10).unwrap() as i64).sum::<i64>();
     println!("{}", ans);
 }
