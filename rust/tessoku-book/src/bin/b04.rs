9f4586346cc9c28968152d13535ab9f42881b6d5
tohshima115 <tohshima115@gmail.com>
2026-05-31 14:33:29 +0900

    feat: b04 2進数→10進数変換を実装

--- rust/tessoku-book/src/bin/b04.rs
+++ rust/tessoku-book/src/bin/b04.rs
@@ -4,10 +4,10 @@ fn main() {
     input! {
         n: String,
     }
-    let mut ans = i64::new;
+    let mut ans: i64 = 0;
     for (i, c) in n.chars().enumerate(){
         if c == '1' {
-            ans += 1 << (n.length - i)
+            ans += 1 << (n.len() -1 - i)
         }
     }
     println!("{}", ans);
