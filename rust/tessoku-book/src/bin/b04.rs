d2442b91eadca13dd743425de307a7b5e1903665
tohshima115 <tohshima115@gmail.com>
2026-05-31 14:21:10 +0900

    checkpoint: 20260531-142110

--- rust/tessoku-book/src/bin/b04.rs
+++ rust/tessoku-book/src/bin/b04.rs
@@ -2,12 +2,14 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: i64,
+        n: String,
     }
     let bit[i64] = &n.iter.
     let mut ans = i64::new;
     for i in 0..bit.length{
-        ans =+ bit[i];
+        if bit[i] == 1 {
+            ans += 1 << i ;
+        }
     }
     println!("{}", ans);
 }
