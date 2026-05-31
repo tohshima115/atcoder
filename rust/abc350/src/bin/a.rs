7a8d570d851cfff0429b8b8d51db433026bcec58
tohshima115 <tohshima115@gmail.com>
2026-05-31 15:36:19 +0900

    checkpoint: 20260531-153619

--- rust/abc350/src/bin/a.rs
+++ rust/abc350/src/bin/a.rs
@@ -4,10 +4,7 @@ fn main() {
     input! {
         s: String,
     }
-    let mut ans = false;
     let num:i64 = s[3..].parse().unwrap();
-    if num >= 1 && num <= 349 && num != 316 {
-        ans = true
-    }
+    let ans = num >= 1 && num <= 349 && num != 316;
     println!("{}", if ans {"Yes"} else {"No"});
 }
