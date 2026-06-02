061eea224f009bab656f8fd6537f926b9676e093
tohshima115 <tohshima115@gmail.com>
2026-06-02 23:17:59 +0900

    feat: K番目の値を取得するための入力パラメータを追加し、出力を整えました

--- rust/contests/drill/src/bin/b3.rs
+++ rust/contests/drill/src/bin/b3.rs
@@ -2,5 +2,11 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        k: usize,
+        mut p: [i64; n],
     }
+    p.sort();
+    let ans = p[p.len() - k];
+    println!("{}", ans);
 }
