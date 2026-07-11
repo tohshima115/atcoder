f4fd646cc7efa7913da397f68ebb98297dd25f53
tohshima115 <tohshima115@gmail.com>
2026-07-11 20:50:55 +0900

    checkpoint: 20260711-205055

--- rust/contests/adt_easy_20260706_1/src/bin/b.rs
+++ rust/contests/adt_easy_20260706_1/src/bin/b.rs
@@ -3,6 +3,16 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
+        s: i64,
+        k: i64,
+        l: [(i64, usize);n]
     }
-    println!("{}", n);
+    let mut sum: i64 = 0;
+    for &(p,q) in &l{
+        sum += p * (q as i64);
+    }
+    if sum < s {
+        sum += k;
+    }
+    println!("{}", sum);
 }
